f66f084fc1065b86c0c00a13d12a0a38c99f3324
Paresh Masade <masade@coherendz.com>
2017-04-25 02:34:04 +0530

    grid editor

--- grid.js
+++ grid.js
@@ -14,163 +14,135 @@ var gridStorage = {
 	localStorage.setItem(STORAGE_KEY, JSON.stringify(rows))
   }
 }
-var	keys = {
-			ESC: 27,
-			TAB: 9,
-			RETURN: 13,
-			LEFT: 37,
-			UP: 38,
-			RIGHT: 39,
-			DOWN: 40
-		};
 
+var	keys = { ESC: 27, TAB: 9, RETURN: 13, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40 };
 
-
-
-
-
+// Data Cell Component
 var datacellComponent = {
 	template 	: '#datacell-template',
-	props 		: ['value','celldata','cellname','cellwidth','noneditable','cellindex'],
+	props 		: ['value','celldata','cellname','widthclass','noneditable','cellindex'],
 	data 		: function(){
-		return {
-			// editme: false
-			// editing: false,
-			// cursor : false,
-		}
+		return {}
 	},
 	computed	: {
-		editableCell: function(){
-			return !this.noneditable;
-		},
-		cellClass : function(){
-			return "col-sm-"+this.cellwidth;
-		},
 		cursor: function(){
-			return (this.$parent.$parent.cursoron == this.cellindex)?true:false;
+			return (this.$parent.cursorcell == this.cellindex)?true:false;
 		},
 		editing: function(){
-			return (this.$parent.$parent.editingcell == this.cellindex)?true:false;
+			return (this.$parent.editingcell == this.cellindex)?true:false;
 		}
 	},
 	methods 	:{
-		editCell: function(value,field){
-			if(this.editableCell){
-				// field.target.focus();
-				// this.editme = true;
-				// console.log(field.currentTarget);
-				// console.log(this.cellindex);
-				this.$parent.$parent.editingcell = this.cellindex;
-			}
+		movecursor: function(){
+			this.$parent.editingcell = null;
+			this.$parent.cursorcell = this.cellindex;
 		},
-		edited: function(value,field){
-			// if(this.editableCell){
-				// field.target.focus();
-				// this.editme = true;
-				// console.log(field.currentTarget);
-				// console.log(this.cellindex);
-				this.$parent.$parent.edited = this.cellindex;
-			// }
-		},
-		doneEdit: function(value,field) {
-			// console.log(!this.$parent.$parent.edited);
-			if(this.$parent.$parent.edited){
-				// console.log(e.keyCode);
-
-				this.$parent.$parent.editingcell = null;
-				this.$parent.$parent.edited = null;
-				this.$emit('input', value);
-			}
-
-		},
-		cancelEdit: function(value,field) {
-			// this.editing = false;
-			this.$parent.$parent.editingcell = null;
-		},
-		moveCursor: function(){
-			this.$parent.$parent.cursoron = this.cellindex;
-			// this.$emit('movecursor',this.cellindex);
-		},
+		editcell:function(){
+			if(!this.noneditable)
+				this.$parent.editingcell = this.cellindex;
+		}
 	},
 	directives: {
-		'cell-focus': {
-			inserted: function (el, value) {
-				this.$parent.$parent.edited = true;
-		  		if (value) {
-					el.getElementsByTagName('input')[0].focus()
-				}
-			}
-		}
 	}
 }
+
+// Data Grid Component
 
 Vue.component('datagrid', {
 	template 	: '#datagrid-template',
 	props 		: ['rows','cols'],
 	components 	: {
 		'datacell' 	: datacellComponent,
-		// 'addrow'	:
 	},
 	data 	: function (){
 		return {
-			newRow: '',
-			cursoron: null,
+			// newRow: '',
+			cursorcell: null,
 			editingcell: null,
-			edited: null,
 		}
 	},
-	computed: {
+	computed   :  {
+		colwidths : function(){
+			var sum = 0; var max = 0; var maxindex = 0;
+			var total = this.cols.reduce(function(total,col) {
+				if(!col.show) return total;
+				return total + (parseInt(col.w) || 10);
+			}, 0);
+			var cws = this.cols.map(function(col,index){
+				if(!col.show) return 0;
+				var cw = Math.ceil((col.w || 10) * 12 / total);
+				if(max < cw){max = cw; maxindex = index;}
+				sum += cw;
+				return cw;
+			})
+
+			cws[maxindex] = (sum>12)?(cws[maxindex] + (12 - sum)) : cws[maxindex];
+
+			return cws.map(function(v){return v?'col-sm-'+v:'';})
+		},
 		totalrows: function(){
 			return this.rows.length;
 		},
 		totalcols: function(){
-			return this.cols.length;
+			return this.cols.reduce(function(count,col) {
+				return count+= (col.show)?1:0;
+			}, 0);
 		}
 	},
-	methods		: {
-		// moveCursor: function(topos){
-			// console.log(topos);
-			// console.log("here");
-		// }
-		addRow: function () {
+	created: function(){
+		window.addEventListener('keydown', event => this.handleKeynav(event));
+	},
+	methods: {
+		handleKeynav : function(e){
+			var preventKeyDefault = false;
+			// console.log(e.which);
+			if(e.which == keys.ESC){ preventKeyDefault= true;}
 
-			var value = this.newRow && this.newRow.trim()
-			if (!value) {
-				return
+			//edit cell on enter
+			if(!this.editingcell && this.cursorcell){
+				if(e.which == keys.RETURN){
+					preventKeyDefault = true;
+					this.editingcell = this.cursorcell;
+				}
 			}
-			this.rows.unshift({
-				id: gridStorage.uid++,
-				name: value,
-				knownfor: '',
-				year: '',
-				degree: '',
-				specialization: ''
-			})
-			this.newRow = ''
-			//
-		},
-		removeAll: function(){
-			this.rows = [];
+
+
+			// Manage cursor movement only when cell not editing
+			if(!this.editingcell){
+				var c = 0; var r = 0;
+				switch(e.which){
+					case keys.LEFT 	:	preventKeyDefault=true;c=-1;r=0;  break;
+					case keys.RIGHT :	preventKeyDefault=true;c= 1;r=0; break;
+					case keys.UP 	:	preventKeyDefault=true;c= 0;r=-1; break;
+					case keys.DOWN 	:	preventKeyDefault=true;c= 0;r=1; break;
+				}
+
+				if(this.cursorcell){
+					var cursor = this.cursorcell.split(',');
+					ri = parseInt(cursor[0])+r; ci= parseInt(cursor[1])+c;
+					ri = (ri<0)?-1:ri;
+					ci = (ri<0)?-1:((ci<0)?0:ci);
+					ri = (ri>=this.totalrows)?this.totalrows-1:ri;
+					ci = (ci>=this.totalcols)?this.totalcols-1:ci;
+				}else{
+					ri =-1; ci=-1;
+				}
+				this.cursorcell = ri+","+ci;
+			}
+
+			if(preventKeyDefault)
+				e.preventDefault();
+
 		}
 	}
+
 });
 
 var app = new Vue({
 
 	data: {
 		rows: gridStorage.fetch(),
-		newRow: '',
-		cursoron: null,
-		editingcell: null,
-		edited: null,
-		cols :[
-			// {m:'id',w:'1',noedit:'true'},
-			{m:'name',w:'3'},
-			{m:'knownfor',w:'3'},
-			{m:'year',w:'1'},
-			{m:'degree',w:'2'},
-			{m:'specialization',w:'2'},
-		],
+		cols : cols,
 	},
 	watch: {
 		rows: {
@@ -180,61 +152,9 @@ var app = new Vue({
 			deep: true
 		}
 	},
-	computed: {
-
-	},
-	filters: {
-		pluralize: function (n) {
-		return n === 1 ? 'row' : 'rows'
-		}
-	},
-	created: function(){
-		window.addEventListener('keydown', event => this.handleKeynav(event));
-	},
+	computed: {},
 	methods: {
-		handleKeynav: function(e) {
-			var preventKey = false;
-			if(e.which == keys.ESC){
-				e.preventDefault();
-			}
-			if(!this.editingcell && this.cursoron){
-				if(e.which == keys.RETURN){
-					e.preventDefault();
-					this.editingcell = this.cursoron;
-					//e.stopPropogation();
-				}
-
-			}
-			if(!this.editingcell){
-				var c = 0; var r = 0;
-				switch(e.which){
-					case keys.LEFT 	:	preventKey=true;c=-1;r=0;  break;
-					case keys.RIGHT :	preventKey=true;c= 1;r=0; break;
-					case keys.UP 	:	preventKey=true;c= 0;r=-1; break;
-					case keys.DOWN 	:	preventKey=true;c= 0;r=1; break;
-					// case keys.TAB 	:	preventKey=true;r= 0; c = (e.shiftKey?-1:+1); break;
-				}
-				if(preventKey)
-					e.preventDefault();
-
-				if(this.cursoron){
-					var cursor = this.cursoron.split(',');
-					ri = parseInt(cursor[0])+r; ci= parseInt(cursor[1])+c;
-					ri = (ri<0)?0:ri;
-					ci = (ci<0)?0:ci;
-					ri = (ri>=this.totalrows)?this.totalrows-1:ri;
-					ci = (ci>=this.totalcols)?this.totalcols-1:ci;
-				}else{
-					ri = 0; ci=0;
-				}
-				this.cursoron = ri+","+ci;
-			}
-			// console.log(e.which,this.editingcell,this.cursoron);
-
-			// console.log(e.which);
-
-		},
-
+		removeAll: function(){ this.rows = [];}
 	}
 });
 
